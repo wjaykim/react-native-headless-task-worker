f3b167cc1088331d6e39aa802ab98963380c6b96
Jay <me@wjay.kim>
2022-02-12 21:11:13 +0900

    chore: initial commit

---
+++ android/src/main/java/com/reactnativeheadlesstaskworker/HeadlessTaskWorkerModule.java
@@ -0,0 +1,34 @@
+package com.reactnativeheadlesstaskworker;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = HeadlessTaskWorkerModule.NAME)
+public class HeadlessTaskWorkerModule extends ReactContextBaseJavaModule {
+    public static final String NAME = "HeadlessTaskWorker";
+
+    public HeadlessTaskWorkerModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    @NonNull
+    public String getName() {
+        return NAME;
+    }
+
+
+    // Example method
+    // See https://reactnative.dev/docs/native-modules-android
+    @ReactMethod
+    public void multiply(int a, int b, Promise promise) {
+        promise.resolve(a * b);
+    }
+
+    public static native int nativeMultiply(int a, int b);
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  HeadlessTaskWorkerExample
+//
+
+import Foundation
